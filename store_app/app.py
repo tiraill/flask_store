f2a068a9848de2348c21c4fb17c144f3293cc056
Матюнин Алексей Сергеевич <Matyunin.A.S@sberbank.ru>
2020-04-18 17:34:28 +0300

    Добавление Sqlalchemy

---
+++ store_app/app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template
+from store_app.models import db, User
+
+
+def create_app():
+    """Returns an initialized Flask application."""
+    app = Flask(__name__)
+    register_extensions(app)
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello World!'
+
+    @app.route('/users')
+    def users():
+        return render_template('users.html', user_list=User.query.all())
+
+    return app
+
+
+def register_extensions(app):
+    """Register extensions with the Flask application."""
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['PROJECT_ROOT'] = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+    db.init_app(app)
