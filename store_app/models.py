ba7adcac9a01224edbcd2a3db08b2acc47fce146
Alesya <alesya.dirsha@gmail.com>
2020-04-19 20:23:57 +0300

    Задача 2. Добавлено описание товара

--- store_app/models.py
+++ store_app/models.py
@@ -14,6 +14,7 @@ class Goods(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=True)
 
     def __repr__(self):
         return '<Good %r>' % self.name
