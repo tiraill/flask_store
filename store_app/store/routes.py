005ee13ef701ebb8ab11d6ef632613b981660bd6
Матюнин Алексей Сергеевич <Matyunin.A.S@sberbank.ru>
2020-05-01 20:06:26 +0300

    fix product naming

--- store_app/store/routes.py
+++ store_app/store/routes.py
@@ -1,11 +1,11 @@
 # from store_app import app, db
 from flask import (
     render_template, Blueprint,
-    # request, redirect, url_for, session, flash, abort
+    request, redirect, url_for, session, flash, abort
 )
 # from flask_login import current_user, login_required
 
-# from app.models import User, Categories, Products,Kart
+from store_app.utils import AddToCart
 from store_app.models import Products
 # from app.admin.forms import Variations
 # import random
@@ -19,14 +19,30 @@ def landing():
 
 @store.route('/goods')
 def goods():
-    return render_template('goods.html', goods_list=Products.query.all())
+    form = AddToCart()
+
+    if form.validate_on_submit():
+        #     cur_user_id = 1
+        #     order = Orders(user_id=cur_user_id, price=0, status='{}')
+        #     db.session.add(order)
+        #     db.session.commit()
+        #
+        #     detail = Order_detail(order_id=order.id, good_id=good_id, quantity=1)
+        #     db.session.add(detail)
+        #     db.session.commit()
+        return redirect(url_for('store.landing'))
+    return render_template(
+        'products.html',
+        form=form,
+        products=Products.query.all()
+    )
 
 
-@store.route('/goods/<int:good_id>', methods=["GET", "POST"])
-def good(good_id: int):
-    # form = AddToCart()
-    #
-    # if form.validate_on_submit():
+@store.route('/product/<int:product_id>', methods=["GET", "POST"])
+def product(product_id: int):
+    form = AddToCart()
+
+    if form.validate_on_submit():
     #     cur_user_id = 1
     #     order = Orders(user_id=cur_user_id, price=0, status='{}')
     #     db.session.add(order)
@@ -35,9 +51,10 @@ def good(good_id: int):
     #     detail = Order_detail(order_id=order.id, good_id=good_id, quantity=1)
     #     db.session.add(detail)
     #     db.session.commit()
-    #     return render_template('goods.html', goods_list=Goods.query.all())
+        return redirect(url_for('store.landing'))
 
     return render_template(
-        'good_template.html',
-        good=Products.query.filter_by(id=good_id).one()
+        'product_template.html',
+        form=form,
+        product=Products.query.filter_by(id=product_id).one()
     )
